8741dde77f543fb46015c3bedf7585d51d7bab1c
AntonBuyanov <102428399+AntonBuyanov@users.noreply.github.com>
2022-09-26 15:22:04 +0600

    fix: presence body, title questions table; refactoring spec

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :answer do
     body { "MyString" }
-    correct { false }
-    question { nil }
+    question
   end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it do
-    should belong_to :question
-  end
+  it { should belong_to :question }
 
   it { should validate_presence_of :body }
 end
