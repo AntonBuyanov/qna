4b2693e2551e1e8067b061e50539199991a1df24
AntonBuyanov <102428399+AntonBuyanov@users.noreply.github.com>
2022-09-26 11:59:14 +0600

    fix: Deleting the correct column in the answer table

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 2022_09_25_090832) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
-    t.boolean "correct"
     t.bigint "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
