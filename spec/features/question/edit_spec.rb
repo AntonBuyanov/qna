7fa067fed5cae92e5dd3f74597cd8addf0cc7e7a
AntonBuyanov <102428399+AntonBuyanov@users.noreply.github.com>
2022-10-16 14:33:50 +0600

    feat: user can edit question with attached files

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -24,11 +24,15 @@ feature 'User can edit his question', %q{
       within '.question' do
         fill_in 'Title', with: 'Test question'
         fill_in 'Body', with: 'text text text'
+
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
         click_on 'Save'
 
         expect(page).to_not have_content question.body
         expect(page).to have_content 'Test question'
         expect(page).to_not have_selector 'textarea'
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
       end
     end
 
