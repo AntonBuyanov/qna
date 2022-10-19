18755908ad392c3a7e3c6850236aa8a2ceac01c2
AntonBuyanov <102428399+AntonBuyanov@users.noreply.github.com>
2022-10-19 12:37:40 +0600

    feat: User can adds multiple link to answer and question

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require('jquery')
+require("@nathanvda/cocoon")
 
 import "bootstrap"
 import "../utilities/answers"
